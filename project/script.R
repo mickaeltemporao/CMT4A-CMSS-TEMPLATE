5221176a506afaf87f44a0e62f4587760cf282fc
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2020-10-30 17:46:54 +0100

    adding install script

---
+++ project/script.R
@@ -0,0 +1,22 @@
+# Requirements
+cran_requirements <- c(
+  "devtools",
+  "formatR",
+  "stargazer",
+  "tidyverse"
+
+)
+
+github_cran_requirements <- c(
+  "gadenbuie/lorem",
+  "jamesmartherus/anesr"
+)
+
+# Create a data frame with installed packages
+current_libs <- as.data.frame(installed.packages())
+to_install   <- setdiff(cran_requirements, current_libs$Package)
+
+# Download and install the remaining packages
+install.packages(to_install, repos = "https://cran.biotools.fr/")
+devtools::install_github(github_cran_requirements)
+
