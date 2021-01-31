49c6a891429b5b5f02ae8f65a987aa4f4d5a610b
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2021-01-31 23:08:10 +0100

    add new vars

--- src/make_data.R
+++ src/make_data.R
@@ -3,7 +3,7 @@ library(lubridate)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Creating a clean dataset
-readRDS("../data/ts_2016.rds") %>% 
+readRDS("../data/ts_2016.rds") %>%
   select(
     V164002,   # date
     V163003,   # region
@@ -11,6 +11,10 @@ readRDS("../data/ts_2016.rds") %>%
     V161031,   # vote intention
     V161155,   # party identification
     V161126,   # ideology
+    V161241,   # religion importance
+    V161082,   # current president approval
+    V162230,   # man works, women home
+    V161309,   # latino
     V161086,   # feeling democrat
     V161087,   # feeling republican
   ) %>%
@@ -21,14 +25,22 @@ readRDS("../data/ts_2016.rds") %>%
     "voting_int"  = "V161031",
     "party_id"    = "V161155",
     "ideology"    = "V161126",
+    "religion"    = "V161241",
+    "pres_appr"   = "V161082",
+    "sexism"      = "V162230",
+    "latino"      = "V161309",
     "feeling_dem" = "V161086",
     "feeling_rep" = "V161087",
-  ) %>% 
+  ) %>%
   filter(
-    age >= 18, 
+    age >= 18,
     between(voting_int, 1, 4),
     between(party_id, 1, 3),
     between(ideology, 1, 7),
+    between(religion, 1, 2),
+    between(pres_appr, 1, 2),
+    between(sexism, 1, 3),
+    between(latino, 1, 2),
     feeling_dem >= 0,
     feeling_rep >= 0,
   ) %>%
@@ -37,4 +49,8 @@ readRDS("../data/ts_2016.rds") %>%
     region     = factor(region,  labels = c("Northeast", "Midwest", "South", "West")),
     voting_int = factor(voting_int, labels = c("H. Clinton", "D. Trump", "G. Johnson", "J. Stein")),
     party_id   = factor(party_id, labels = c("Democrat", "Republican", "Independent")),
+    religion   = factor(religion, labels = c("Important", "Not important")),
+    pres_appr  = factor(pres_appr, labels = c("Approve", "Disapprove")),
+    sexism     = factor(sexism, labels = c("Better", "Worse", "Makes no difference")),
+    latino     = factor(latino, labels = c("Yes", "No")),
   ) %>% saveRDS("../data/clean_2016.rds")
